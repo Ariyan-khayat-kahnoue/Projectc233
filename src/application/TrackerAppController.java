becb21d2dcc633075b93f2941d4c72ede7bc8601
Ariyan khayat kahnoue <ariyan.khayatkahnouei@ucalgary.ca>
2022-08-12 21:53:10 -0600

    Heres the code

--- src/application/TrackerAppController.java
+++ src/application/TrackerAppController.java
@@ -105,18 +105,18 @@ public class TrackerAppController{
 			TeamTwo.getItems().addAll("Real Madrid","FC Barcelona","Atletico","Valencia CF");
 			counter++;
 		}
-		else if("MLS" == getLeagueValue){
+		else if("MLS".equals(getLeagueValue)){
 			TeamOne.getItems().addAll("Philadelphia","New York City","Montreal","New York");
 			TeamTwo.getItems().addAll("Philadelphia","New York City","Montreal","New York");
 			counter++;
 			
 		}
-		else if("Premier league" == getLeagueValue){
+		else if("Premier league".equals(getLeagueValue)){
 			TeamOne.getItems().addAll("Tottenham","Arsenal","Liverpool","Manchester United F.C.");
 			TeamTwo.getItems().addAll("Tottenham","Arsenal","Liverpool","Manchester United F.C.");
 			counter++;
 		}
-		else if("Bundesliga" == getLeagueValue){
+		else if("Bundesliga".equals(getLeagueValue)){
 			TeamOne.getItems().addAll("B. Munich","Dortmund","SV Werder Bremen","Hamburger SV");
 			TeamTwo.getItems().addAll("B. Munich","Dortmund","SV Werder Bremen","Hamburger SV");
 			counter++;
@@ -125,6 +125,7 @@ public class TrackerAppController{
 		else {
 			
 		}
+		
 	}
 	
 
@@ -132,8 +133,10 @@ public class TrackerAppController{
 //	players.addA(Arrays.asList(T1player1,T1player2,T1player3,T1player4,T1player5,T1player6,T1player7,T1player8,T1player9,T1player10));
 //	ArrayList<CheckBox> players = new ArrayList<CheckBox>();
 
-	String[] RealMadrid = new String[]{"Courtois","Carvajal","Nacho","Alaba","Kroos","Modric","Hzard","Benzema","Asensio","Casemiro"};
-	String[] FC_Barscelona = new String[] {};
+	String[] RealMadrid = new String[]{"Courtois","Carvajal","Nacho","Alaba","Kroos","Modric","Casemiro","Benzema","Asensio","Hzard"};
+	String[] FC_Barscelona = new String[] {"Ter Stegen","Jordi Alba","Pique","Garcia","Busquets","S.Roberto","Lewandowski","Raphinha","Dembele"};
+	String[] Atletico_de_Madrid = {"Ivo Grbic","Giménez","Renan Lodi","Nahuel Molina","Kondogbia","Llorente","Thomas Lemar","João Félix","Griezmann","Cunha"};
+	
 	@FXML
 	private void ConfirmTeams(ActionEvent event) throws IOException {
 		String TeamOneName = TeamOne.getValue();
@@ -141,8 +144,9 @@ public class TrackerAppController{
 		if (TeamOneName != TeamTwoName) {
 			VBoxTeam1.setVisible(true);
 	        VBoxTeam2.setVisible(true);
+	        
 //		for(int Index = 0;Index!=9;Index++) {
-			if (TeamOneName == "Real Madrid") {
+			if (TeamOneName == "Real Madrid"||TeamTwoName=="Real Madrid") {
 				T1player1.setText(RealMadrid[0]);
 				T1player2.setText(RealMadrid[1]);
 				T1player3.setText(RealMadrid[2]);
@@ -153,7 +157,7 @@ public class TrackerAppController{
 				T1player8.setText(RealMadrid[7]);
 				T1player9.setText(RealMadrid[8]);
 				T1player10.setText(RealMadrid[9]);
-			if(TeamOneName == "FC_Barscelona"||TeamTwoName=="FC_Barscelona") {
+			if(TeamOneName == "FC Barscelona"||TeamTwoName=="FC Barscelona") {
 				T1player1.setText(FC_Barscelona[0]);
 				T1player2.setText(FC_Barscelona[1]);
 				T1player3.setText(FC_Barscelona[2]);
@@ -165,12 +169,23 @@ public class TrackerAppController{
 				T1player9.setText(FC_Barscelona[8]);
 				T1player10.setText(FC_Barscelona[9]);
 			}
-				
+			if(TeamOneName == "Atletico"||TeamTwoName=="Atletico") {
+				T1player1.setText(Atletico_de_Madrid[0]);
+				T1player2.setText(Atletico_de_Madrid[1]);
+				T1player3.setText(Atletico_de_Madrid[2]);
+				T1player4.setText(Atletico_de_Madrid[3]);
+				T1player5.setText(Atletico_de_Madrid[4]);
+				T1player6.setText(Atletico_de_Madrid[5]);
+				T1player7.setText(Atletico_de_Madrid[6]);
+				T1player8.setText(Atletico_de_Madrid[7]);
+				T1player9.setText(Atletico_de_Madrid[8]);
+				T1player10.setText(Atletico_de_Madrid[9]);
+			}	
 			}	
 			
 		}
 		else {
-			ErrorLabel.setText("You can't have Two "+TeamOneName+" Please pick a diffrent Teams");
+			ErrorLabel.setText("You can't have Two "+TeamOneName+"s Please pick diffrent Teams");
 		}
 	}	
 		
